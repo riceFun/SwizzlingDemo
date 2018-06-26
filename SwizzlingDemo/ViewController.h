2f23e70b4e2af1deeaf35f371db76f1d9833cf2c
riceFun <ricefun@ricefundeimac.local>
2018-06-26 15:21:34 +0800

    Initial Commit

---
+++ SwizzlingDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SwizzlingDemo
+//
+//  Created by riceFun on 2018/6/26.
+//  Copyright © 2018年 riceFun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SwizzlingDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SwizzlingDemo
+//
+//  Created by riceFun on 2018/6/26.
+//  Copyright © 2018年 riceFun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
